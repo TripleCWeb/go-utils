08ba49b012e20f3ffacf4069209ac37b6625d06b
Jay <zhanchenjin.2008@163.com>
2024-07-10 10:44:36 +0800

    + 增加时间接口

--- time.go
+++ time.go
@@ -1,6 +1,9 @@
 package utils
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 // IsToday按照utc0时区来判断自然天
 func IsTodayUTC(ts int32) bool {
@@ -25,4 +28,37 @@ func NextMonthsFirstTimeStamp(ts int64, months int) int64 {
 func YesterdayLastTS() int64 {
 	now := time.Now()
 	return time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).Unix() - 1
+}
+
+func StringToTime(str string) (*time.Time, error) {
+	if str == "" {
+		return nil, nil
+	}
+	// 将字符串解析为 int64 类型的时间戳
+	timestamp, err := strconv.ParseInt(str, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	// 使用 time.Unix() 将时间戳转换为 time.Time 类型
+	t := time.Unix(timestamp, 0)
+
+	return &t, nil
+}
+
+// StringToTimestamp将时间字符串解析为时间戳
+// 输入参数：
+// timeStr: 时间字符串:"2024-06-06T1:38:32.541+08:00"
+// format: 时间格式:"2006-01-02T15:04:05.999-07:00"
+func StringToTimestamp(timeStr, format string) (int64, error) {
+	// 解析时间字符串为 time.Time 类型
+	t, err := time.Parse(format, timeStr)
+	if err != nil {
+		return 0, err
+	}
+
+	// 将 time.Time 类型的时间转换为时间戳
+	timestamp := t.Unix()
+
+	return timestamp, nil
 }
